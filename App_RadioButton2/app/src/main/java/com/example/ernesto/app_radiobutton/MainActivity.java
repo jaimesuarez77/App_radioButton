9b49ed73e75ae05ac9c2415cc2f218750819718f
jaimesuarez77 <62628107+jaimesuarez77@users.noreply.github.com>
2020-04-24 18:47:38 -0500

    added more file

---
+++ App_RadioButton2/app/src/main/java/com/example/ernesto/app_radiobutton/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.ernesto.app_radiobutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1, et2;
+    private TextView tv1;
+    private RadioButton rb1, rb2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = (EditText)findViewById(R.id.txt_valor1);
+        et2 = (EditText)findViewById(R.id.txt_valor2);
+        tv1 = (TextView)findViewById(R.id.textView);
+        rb1 = (RadioButton)findViewById(R.id.rb_sumar);
+        rb2 = (RadioButton)findViewById(R.id.rb_restar);
+    }
+
+    //Método para el botón calcular
+    public void Calcular(View view){
+        String valor1_String = et1.getText().toString();
+        String valor2_Sgtring = et2.getText().toString();
+
+        int valor1_int = Integer.parseInt(valor1_String);
+        int valor2_int = Integer.parseInt(valor2_Sgtring);
+
+        if(rb1.isChecked() == true){
+            int suma = valor1_int + valor2_int;
+            String resultado = String.valueOf(suma);
+            tv1.setText(resultado);
+        } else if(rb2.isChecked() == true){
+            int resta = valor1_int - valor2_int;
+            String resultado = String.valueOf(resta);
+            tv1.setText(resultado);
+        }
+    }
+}
